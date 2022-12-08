a25172a8515ef1388796e9716c8606eccec494a0
adpol <adpol95@gmail.com>
2022-12-08 20:19:03 +0300

    Completely rework logic of background change(day/night theme). Transitions  now smooth. Created solid base to adapt new elements on the page way easier.

--- controls.js
+++ controls.js
@@ -1,9 +1,12 @@
 const bodyBridge = document.getElementsByTagName('body')[0];
-const searchHeadBridge = document.getElementById('searchArea').style;
-const searchAreaHeadDivBorderBridge = document.getElementById('dividerArea').style;
+const searchHeadBridge = document.getElementsByClassName('searchArea')[0].style;
+const searchAreaHeadDivBorderBridge = document.getElementsByClassName('dividerArea')[0].style;
 const darkThemeImg = [...document.getElementsByClassName('darkTheme')];
 const whiteThemeImg = [...document.getElementsByClassName('whiteTheme')];
 let stateOfTheme = false;
+
+const arrTest1 = [searchHeadBridge, searchAreaHeadDivBorderBridge]
+
 //const currentHour = new Date().getHours();
 
 // if (currentHour >= 8 && currentHour < 17) {
@@ -15,41 +18,15 @@ let stateOfTheme = false;
 // }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const dayNight = () => {
-    searchHeadBridge.background = stateOfTheme ? "#fafbfb" : "#0d1213";
-    searchHeadBridge.border = stateOfTheme ? "1px #6d7175 solid" : "1px solid #59767a";
-    searchAreaHeadDivBorderBridge.border = stateOfTheme ? "1px solid #6d7175": "1px solid #59767a";
-
-    if (!stateOfTheme) {
-        bodyBridge.setAttribute('class', 'darkThemeBody');
-        whiteThemeImg.map((el, i) => {
-            darkThemeImg[i].style.display = "inline-block";
-            return el.style.display = "none"
-        });
-        stateOfTheme = !stateOfTheme;
-
-    } else {
-        bodyBridge.setAttribute('class', '');
-        whiteThemeImg.map((el, i) => {
-            darkThemeImg[i].style.display = "none";
-            return el.style.display = "inline-block"
-        });
-        stateOfTheme = !stateOfTheme;
-    }
+    arrTest1.forEach(el => {
+        el.background = stateOfTheme ? "#fafbfb" : "#0d1213";
+        el.border = stateOfTheme ? "1px #6d7175 solid" : "1px solid #59767a";
+    })
+    bodyBridge.setAttribute('class', stateOfTheme ? '' : 'darkThemeBody');
+    whiteThemeImg.map((el, i) => {
+        darkThemeImg[i].style.opacity = stateOfTheme ? "0" : "1";
+        return el.style.opacity = stateOfTheme ? "1" : "0";
+    });
+    stateOfTheme = !stateOfTheme;
 }
